b75eb6db72928697b4c8a1176eb828e3e3a98d4e
Leila <nb.leila10@gmail.com>
2019-04-02 09:30:30 -0500

    Add check to be sure username, password, and department are provided

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,27 +22,32 @@ function generateToken(user) {
 router.post("/api/register", (req, res) => {
   const user = req.body
 
-  //generate hash from user's password
-  const hash = bcrypt.hashSync(user.password, 10) //2 ^ n times
-
-  //override use.password with hash
-  user.password = hash
-
-  //Create token with user info
-  const token = generateToken(user)
-
-  // Add user to database and send back response, with token info
-  Users.add(user)
-    .then(userId => {
-      res.status(201).json({ userId: userId, token: token })
+  if (!user.username || !user.password || !user.department) {
+    res.status(400).json({
+      errorMessage: "Please provide a username, password, and department."
     })
-    .catch(error => {
-      res
-        .status(500)
-        .json({
+  } else {
+    //generate hash from user's password
+    const hash = bcrypt.hashSync(user.password, 10) //2 ^ n times
+
+    //override use.password with hash
+    user.password = hash
+
+    //Create token with user info
+    const token = generateToken(user)
+
+    // Add user to database and send back response, with token info
+    Users.add(user)
+      .then(userId => {
+        res.status(201).json({ userId: userId, token: token })
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({
           error: "There was an error saving the new user to the database"
         })
-    })
+      })
+  }
 })
 
 module.exports = router
